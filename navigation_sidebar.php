fb9b148375da8627fa8d3235d1bdc16faf1f5487
danialrosli534 <danialrosli534@gmail.com>
2024-06-09 01:53:57 +0800

    Update navigation_sidebar.php

--- navigation_sidebar.php
+++ navigation_sidebar.php
@@ -60,7 +60,12 @@
 
     <a class="nav-button btn">
         <i class="bi bi-arrow-right"></i>
-        <div>New Menu</div>
+        <div>Accident Report</div>
+    </a>
+
+    <a class="nav-button btn">
+        <i class="bi bi-arrow-right"></i>
+        <div>Traffic Summon Enforcement</div>
     </a>
 <!--    <a class="nav-button btn">-->
 <!--        <i class="bi bi-arrow-right"></i>-->
