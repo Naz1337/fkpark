5405bda6505b0c985091b875de32f21ec051a803
danialrosli534 <danialrosli534@gmail.com>
2024-06-09 12:39:13 +0800

    Create accident_report.php

---
+++ accident_report.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once 'layout_top.php';
+
+if (!isset($_SESSION['username'])) {
+    to_url('login.php');
+}
+
+?>
+
+<h1>User Dashboard</h1>
+<p>Logged in as: <?= $_SESSION['username'] ?></p>
+
+<?php
+require_once 'layout_bottom.php';
