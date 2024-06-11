e359267ab56ae1630bc3934c78c475551a6932df
haikalimran21 <165311975+haikalimran21@users.noreply.github.com>
2024-06-11 13:45:41 +0800

    Update indexx.php

--- indexx.php
+++ indexx.php
@@ -4,7 +4,7 @@ include 'phpqrcode/qrlib.php';
 $reservation_id = isset($_GET['text']) ? $_GET['text'] : '';
 
 // Create the URL that the QR code should redirect to
-$url = "http://localhost/fkpark/view_reservation.php?id=" . urlencode($reservation_id);
+$url = "https://indah.ump.edu.my/CB22159/fkpark/view_reservation.php?id=" . urlencode($reservation_id);
 
 // Generate the QR code
 QRcode::png($url);
