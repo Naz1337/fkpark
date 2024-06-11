ceefa63ab23aebe3d8f18ffddb8b1e9dc111f1f6
danialrosli534 <danialrosli534@gmail.com>
2024-06-11 17:33:44 +0800

    summon

--- navigation_sidebar.php
+++ navigation_sidebar.php
@@ -68,7 +68,7 @@
 
     <h6 class="text-uppercase fw-bold mb-2 mt-5 mx-3">Traffic Summon</h6>
 
-    <a class="nav-button btn" href="traffic_dashboard.php">
+    <a class="nav-button btn" href="traffic_dash.php">
         <i class="bi bi-arrow-right"></i>
         <div>Traffic Dashboard</div>
     </a>
