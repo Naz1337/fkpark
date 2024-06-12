389455c27b808bef9ad8710bc1de5e3665819fa0
danialrosli534 <danialrosli534@gmail.com>
2024-06-12 13:14:10 +0800

    Update navigation_sidebar.php

--- navigation_sidebar.php
+++ navigation_sidebar.php
@@ -70,9 +70,9 @@
 
     <h6 class="text-uppercase fw-bold mb-2 mt-5 mx-3">Traffic Summon</h6>
 
-    <a class="nav-button btn" href="trafic_dash.php">
+    <a class="nav-button btn" href="trafic_enforcement.php">
         <i class="bi bi-arrow-right"></i>
-        <div>Dashboard</div>
+        <div>Traffic Enforcement</div>
     </a>
     <a class="nav-button btn" href="traffic_summon.php">
         <i class="bi bi-arrow-right"></i>
