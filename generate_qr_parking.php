64b400733a4ca3470964ab0ca556dae15397b924
Fawwaz <mohdfawwazhatmi@gmail.com>
2024-06-12 12:58:58 +0800

    Update generate_qr_parking.php

--- generate_qr_parking.php
+++ generate_qr_parking.php
@@ -6,12 +6,10 @@ $space_id = isset($_GET['text']) ? $_GET['text'] : '';
 
 // Create the URL that the QR code should redirect to
 if ($_SERVER['APP_ENV'] !== 'development')
-    $uri = '/CB22159/fkpark/view_qr_parking.php?text=' . $space_id;
+    $uri = '/CB22159/fkpark/view_qr_parking.php?id=' . $space_id;
 else
-    $uri = '/fkpark/view_gr_parking.php?text=' . $space_id;
+    $uri = '/fkpark/view_qr_parking.php?id=' . $space_id;
 
-
-// $url = "https://indah.ump.edu.my/CB22159/fkpark/view_qr_parking.php?id=" . urlencode($space_id);
 
 // Generate the QR code
 QRcode::png(get_base_url().$uri);
