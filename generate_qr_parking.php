2b33e9b27e5d8845b2b764f8bb10b4eaac9b085e
Fawwaz <mohdfawwazhatmi@gmail.com>
2024-06-12 09:44:14 +0800

    update

---
+++ generate_qr_parking.php
@@ -0,0 +1,10 @@
+<?php
+include 'phpqrcode/qrlib.php';
+
+$reservation_id = isset($_GET['text']) ? $_GET['text'] : '';
+
+// Create the URL that the QR code should redirect to
+$url = "https://indah.ump.edu.my/CB22159/fkpark/view_qr_parking.php?id=" . urlencode($reservation_id);
+
+// Generate the QR code
+QRcode::png($url);
